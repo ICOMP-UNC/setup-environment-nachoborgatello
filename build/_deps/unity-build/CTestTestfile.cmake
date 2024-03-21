630699c873a3a4ca210099e43095829b3f88378e
Nacho Borgatello <81312177+nachoborgatello@users.noreply.github.com>
2024-03-21 17:55:57 -0300

    Delete build directory

--- build/_deps/unity-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nacho/Desktop/setup-environment-nachoborgatello/external/Unity
-# Build directory: /home/nacho/Desktop/setup-environment-nachoborgatello/build/_deps/unity-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
