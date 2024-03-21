be8d0571de2d1c725251a1db6d2ac1eaa5eead35
Nacho Borgatello <81312177+nachoborgatello@users.noreply.github.com>
2024-03-21 17:56:28 -0300

    Delete build directory

--- build/lib/libdyn/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nacho/Desktop/setup-environment-nachoborgatello/lib/libdyn
-# Build directory: /home/nacho/Desktop/setup-environment-nachoborgatello/build/lib/libdyn
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lib/libsta/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nacho/Desktop/setup-environment-nachoborgatello/lib/libsta
-# Build directory: /home/nacho/Desktop/setup-environment-nachoborgatello/build/lib/libsta
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
